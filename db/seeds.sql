ac017b0c96677460616194d6b155a05b07884417
Ash <Ashleigh.lund@gmail.com>
2019-07-13 17:00:32 -0500

    set up files for app

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name) VALUES ('Pub Burger');
+INSERT INTO burgers (burger_name) VALUES ('Impossible Burger');
+INSERT INTO burgers (burger_name)  VALUES ('Mushroom Swiss');
